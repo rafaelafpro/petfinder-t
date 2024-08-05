d0c217ad4f68bf7ce0d700573f94a6882aaf17c0
Rafael <rafaelafpro@gmail.com>
2024-08-05 16:11:33 -0300

    nunjucks path

--- src/server.js
+++ src/server.js
@@ -1,30 +1,28 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const server = express();
+const db = require("./database/db");
 
-const server = express()
-const db = require('./database/db')
+server.use(express.static("public"));
 
-server.use(express.static('public'))
+server.use(express.urlencoded({ extended: true }));
 
-server.use(express.urlencoded({ extended: true }))
+const nunjucks = require("nunjucks");
+nunjucks.configure(path.join(__dirname, "views"), {
+  express: server,
+  noCache: true,
+});
 
-const nunjucks = require('nunjucks')
-nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
+server.get("/", (req, res) => {
+  return res.render("index.html");
+});
 
-server.get('/', (req, res) => {
-    return res.render("index.html")
-})
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
 
-
-server.get('/create-point', (req, res) => {
-    return res.render("create-point.html")
-})
-
-server.post('/savepoint', (req,res) => {
-
-    const query = `
+server.post("/savepoint", (req, res) => {
+  const query = `
     INSERT INTO places (
         image,
         name,
@@ -34,53 +32,51 @@ server.post('/savepoint', (req,res) => {
         city,
         items
     ) VALUES (?, ?, ?, ?, ?, ?, ?);
-`
+`;
 
-const values = [
+  const values = [
     req.body.image,
     req.body.name,
     req.body.adress,
     req.body.adress2,
     req.body.state,
     req.body.city,
-    req.body.items
-]
+    req.body.items,
+  ];
 
-function afterInsertData(err) {
+  function afterInsertData(err) {
     if (err) {
-        console.log(err)
-        return res.render('create-point.html', {notsaved: true})
+      console.log(err);
+      return res.render("create-point.html", { notsaved: true });
     }
 
-    console.log("Cadastrado com sucesso")
-    console.log(this)
+    console.log("Cadastrado com sucesso");
+    console.log(this);
 
-    return res.render('create-point.html', {saved
-    : true})
-}
+    return res.render("create-point.html", { saved: true });
+  }
 
-db.run(query, values, afterInsertData)
-})
+  db.run(query, values, afterInsertData);
+});
 
-server.get('/search', (req, res) => {
+server.get("/search", (req, res) => {
+  const search = req.query.search;
 
-    const search = req.query.search
+  if (search == "") {
+    return res.render("search.html", { total: 0 });
+  }
 
-    if(search == ''){
-        return res.render("search.html", {total: 0})
+  db.all(
+    `SELECT * FROM places WHERE city LIKE '%${search}%'`,
+    function (err, rows) {
+      if (err) {
+        return console.log(err);
+      }
+
+      const total = rows.length;
+      return res.render("search.html", { places: rows, total });
     }
+  );
+});
 
-
-    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
-        if (err) {
-             return console.log(err)
-        }
-
-        const total = rows.length
-        return res.render("search.html", { places: rows, total })
-    })
-})
-
-    
-
-server.listen(process.env.PORT || 3333)
+server.listen(process.env.PORT || 3333);
